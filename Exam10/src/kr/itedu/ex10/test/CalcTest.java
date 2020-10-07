c95d51df7d8ac0c3e09a41b9050e34bcc04b40c4
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-10-07 10:43:06 +0900

    Test method sum

--- Exam10/src/kr/itedu/ex10/test/CalcTest.java
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -8,9 +8,15 @@ import kr.itedu.ex10.Calc;
 
 class CalcTest {
 	@Test
-	void testSum() {		
-		assertEquals(5, Calc.sum(2, 3));
-		assertEquals(9, Calc.sum(4, 5));
+	void testA() {
+		assertEquals(5, Calc.sum(2, 3));				
+	}
+	@Test
+	void testB() {		
+		assertEquals(9, Calc.sum(4, 5));				
+	}
+	@Test
+	void testC() {		
 		assertEquals(13, Calc.sum(7, 6));		
 	}
 }
